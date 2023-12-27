a5bb19d73428ef302e787368ea0b78ca4ee7b229
SabadoCibertec2023 <i202218101@cibertec.edu.pe>
2023-12-27 09:07:41 -0500

    ProyectoCrudSpring2

---
+++ src/main/java/com/sistemas/demo/controler/Controlador.java
@@ -0,0 +1,56 @@
+package com.sistemas.demo.controler;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sistemas.demo.interfaceService.IpersonaService;
+import com.sistemas.demo.modelo.Persona;
+
+@Controller
+@RequestMapping
+public class Controlador {
+
+	@Autowired
+	private IpersonaService service;
+	
+	@GetMapping("/listar")
+	public String listar(Model model) {
+		List<Persona> personas = service.listar();
+		model.addAttribute("personas", personas);
+		return "index";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable int id, Model model) {
+		Optional<Persona> persona = service.listarId(id);
+		model.addAttribute("persona",persona);
+		return "form";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(Model model, @PathVariable int id) {
+		service.eliminar(id);
+		return "redirect:/listar";
+	}
+	
+	@GetMapping("/new")
+	public String agregar(Model model) {
+		model.addAttribute("persona", new Persona());
+		return "form";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@Validated Persona p, Model model) {
+		service.guardar(p);
+		return "redirect:/listar";
+	}
+}
---
+++ src/main/java/com/sistemas/demo/modelo/Persona.java
@@ -0,0 +1,73 @@
+package com.sistemas.demo.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="persona")
+public class Persona {
+	
+	//Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	
+	
+	//Constructores
+	public Persona() {
+		
+	}
+
+	public Persona(int id, String nombre, String apellido, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+	}
+	
+	//Metodos Getters y Setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+
+}
